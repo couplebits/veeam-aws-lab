c4bb2520cfd8f54df7908001bb59e3a251431972
Eric Ellenberg <eric@couplebits.com>
2022-03-10 16:37:49 -0500

    Adding flex 2.1 session changes
    Flex 2.1 sessions removes the KMS CMK lab sections. Updated templates and deploy script to reflect the removal of the CMK section.
    
    Signed-off-by: Eric Ellenberg <eric@couplebits.com>

---
+++ deploy-flex21.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# REQUIREMENTS
+# AWS CLI v2 - https://github.com/aws/aws-cli/tree/v2
+
+# Extract the list of attendees from the attendees file.
+attendeesFile=attendees.txt
+attendees=$(cat $attendeesFile)
+
+# Specify the 12-digit AWS account ID of the production account.
+ProductionAccountId=
+
+# Specify the region where you are deploying the master template.
+PrimaryRegionId=us-east-1
+
+# Specify the secondary region you will use for the lab
+SecondaryRegionId=us-west-1
+
+# Iterator variable to avoid deploying more than 8 labs at a time (avoids AWS rate limits)
+i=1
+
+# Deploy the stack for each attendee. On each 8th attendee's deployment, the script will wait until their deployment is complete before proceeding to avoid exceeding AWS rate limits.
+for attendee in $attendees
+do
+    if [ $i == 8 ]
+    then
+        echo "Deploying lab for $attendee..." >> logs.txt && stack=$(aws cloudformation create-stack --capabilities CAPABILITY_IAM --template-url https://veeam-aws-cloudformation.s3.amazonaws.com/veeam-aws-lab/vbaws-lab-backup-primary-master-flex2.1.template --stack-name $attendee-backup-primary --output text --query "StackId" --region $PrimaryRegionId --parameters ParameterKey=UserName,ParameterValue=$attendee ParameterKey=ProductionAccountId,ParameterValue=$ProductionAccountId ParameterKey=PrimaryRegionId,ParameterValue=$PrimaryRegionId ParameterKey=SecondaryRegionId,ParameterValue=$SecondaryRegionId) && echo "Attendee lab stack ID is $stack" >> logs.txt && echo "Deployed stack for $attendee. Waiting until stack deployment is complete before proceeding..." >> logs.txt && aws cloudformation wait stack-create-complete --stack-name $stack && i=1
+    else
+        echo "Deploying lab for $attendee..." >> logs.txt && stack=$(aws cloudformation create-stack --capabilities CAPABILITY_IAM --template-url https://veeam-aws-cloudformation.s3.amazonaws.com/veeam-aws-lab/vbaws-lab-backup-primary-master-flex2.1.template --stack-name $attendee-backup-primary --output text --query "StackId" --region $PrimaryRegionId --parameters ParameterKey=UserName,ParameterValue=$attendee ParameterKey=ProductionAccountId,ParameterValue=$ProductionAccountId ParameterKey=PrimaryRegionId,ParameterValue=$PrimaryRegionId ParameterKey=SecondaryRegionId,ParameterValue=$SecondaryRegionId) && echo "Attendee lab stack ID is $stack" >> logs.txt && echo "Deployed stack for $attendee. Sleeping 10 seconds before next deployment..." >> logs.txt && i=$((i + 1)) && sleep 10
+    fi
+done
